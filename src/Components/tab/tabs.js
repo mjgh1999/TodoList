c8846df4fa24477229fe0586253694633b86bfbd
mjgh1999 <80089637+mjgh1999@users.noreply.github.com>
2022-04-20 15:35:47 +0430

    define tab component in todo list

--- src/Components/tab/tabs.js
+++ src/Components/tab/tabs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Tabs,Divider, Input, Button ,Row, Col ,Typography, Space} from 'antd';
+import TaskIteam from '../task/taskItem';
 
 const { TabPane } = Tabs;
 
@@ -12,8 +13,13 @@ function TabView (){
     return (
                 <Tabs type="card" defaultActiveKey="1" onChange={callback} centered >
                     <TabPane tab="Todos" key="1">
+                    <TaskIteam></TaskIteam>
+                    <TaskIteam></TaskIteam>
+                    <TaskIteam></TaskIteam>
+                    <TaskIteam></TaskIteam>
                     </TabPane>
                     <TabPane tab="Done" key="2">
+                    <TaskIteam></TaskIteam>
                     </TabPane>
                 </Tabs>
     );
