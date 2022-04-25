68f1130ec8e913d667cb0077d56d508f9b3aabca
mjgh1999 <80089637+mjgh1999@users.noreply.github.com>
2022-04-26 02:02:37 +0430

    add done undon edit delete and save button function properly without api

--- src/Components/tab/tabs.js
+++ src/Components/tab/tabs.js
@@ -1,27 +1,103 @@
-import React from 'react';
-import { Tabs,Divider, Input, Button ,Row, Col ,Typography, Space} from 'antd';
+import React ,{useState} from 'react';
+import { Tabs,Layout,Divider, Input, Button ,Row, Col ,Typography, Space} from 'antd';
 import TaskIteam from '../task/taskItem';
-
+import PageHeader from '../header/header.js';
+import Item from 'antd/lib/list/Item';
+const { Header, Content } = Layout;
+const { Text, Link ,Title} = Typography;
 const { TabPane } = Tabs;
 
-function callback(key) {
-  console.log(key);
-}
+
+
+
+
+
 
 function TabView (){
-  
-    return (
-                <Tabs type="card" defaultActiveKey="1" onChange={callback} centered >
-                    <TabPane tab="Todos" key="1">
-                    <TaskIteam></TaskIteam>
-                    <TaskIteam></TaskIteam>
-                    <TaskIteam></TaskIteam>
-                    <TaskIteam></TaskIteam>
+
+    const [text,setText] = useState('')
+    const [todos,setTodos]=useState([])
+
+    let addButtonHandler= e =>{
+      if(text){
+      setTodos([...todos,{key:Date.now(),done:false,text}]);
+      setText('');}
+    }
+
+    let inputHandler= e =>setText(e.target.value)
+
+    let doneTodoList = todos.filter(item=>item.done===true)
+    let unDoneTodoList = todos.filter(item=>item.done===false)
+
+    let deleteTodo = key =>{
+      setTodos(todos.filter(item => item.key != key))
+    }
+
+    let toggleTodo = key =>{
+      let targetTodo = todos.find(item => item.key===key);
+      targetTodo.done = !targetTodo.done
+      let newTodoList = todos.filter(item => item.key != key)
+      setTodos([...newTodoList,targetTodo])
+    }
+
+    let editTodo = (key,newText) =>{
+      let targetTodo = todos.find(item => item.key===key);
+      targetTodo.text = newText
+      let newTodoList = todos.filter(item => item.key != key)
+      setTodos([...newTodoList,targetTodo])
+    }
+      
+
+    return (    
+    
+    <>
+                <Layout >
+                  <Content >
+                    <Space direction="vertical">
+                    <Title></Title>
+                    <Title>Welcome!</Title>
+                    <Text>To get started, add some items to your list:</Text>
+                    <Space direction="horizontal">
+                        <Input placeholder="i want to do ..." value={text} onChange={inputHandler} />
+                        <Button type="primary" onClick={addButtonHandler}>add</Button>
+                    </Space>
+                    
+                    </Space>
+                    <Divider></Divider>
+
+                    <Tabs type="card" defaultActiveKey="1"  centered >
+                    <TabPane tab={unDoneTodoList.length +' Todos '} key="1">
+
+                      {
+                        unDoneTodoList.length === 0
+                          ? <Text>There is no todo </Text>
+                          : unDoneTodoList.map(item=><TaskIteam 
+                                                        key={item.key} 
+                                                        item={item} 
+                                                        delete={deleteTodo} 
+                                                        toggleaction={toggleTodo} 
+                                                        edit={editTodo}/>)
+                      }
+
                     </TabPane>
-                    <TabPane tab="Done" key="2">
-                    <TaskIteam></TaskIteam>
-                    </TabPane>
+                    <TabPane tab={doneTodoList.length +' Done '} key="2">
+                    {
+                        doneTodoList.length === 0
+                          ? <Text>There is no done task </Text>
+                          : doneTodoList.map(item=><TaskIteam 
+                                                      key={item.key} 
+                                                      item={item} 
+                                                      delete={deleteTodo} 
+                                                      toggleaction={toggleTodo} 
+                                                      edit={editTodo}/>)
+                      }
+                      </TabPane>
                 </Tabs>
+                    
+            </Content>
+        </Layout>
+                
+                </>
     );
       }
     
--- src/Components/task/taskItem.js
+++ src/Components/task/taskItem.js
@@ -1,39 +1,74 @@
-import React from 'react';
-import { Card ,Row,Col,Button,Typography,Space } from 'antd';
+import React,{useState} from 'react';
+import { Card ,Row,Col,Button,Typography,Space,Input } from 'antd';
 import '../task/taskStyle.css';
 const { Title, Paragraph, Text, Link } = Typography;
 
 
 
 
-function TaskIteam (){
+function TaskIteam (probs){
+
+    const [editstatus,setEditStatus] = useState(false)
+    const [editText,setEditText] = useState(probs.item.text)
+    let inputHandler = e => setEditText(e.target.value)
+
+    const saveEditHandler = text =>{
+        probs.edit(probs.item.key,text);
+        setEditStatus(false);
+    }
   
     return (
-        
-                <Row justify="space-around" align="middle" className='task-box'>
-                        
-                        <Col xs={24} sm={24} md={14} lg={16} xl={12}>
-                    
-                            <p className='task-text'>this is a task</p>
-                            
-                        </Col>
+        <>
+               
+            {
+                        !editstatus
+                           ?(
+                            <Row justify="space-around" align="middle" className='task-box'>
+                                <Col xs={24} sm={24} md={14} lg={16} xl={12}>
+                                    <p className='task-text'>{probs.item.text}</p>
+                                </Col>
 
-                        <Space align='center'>
-                            <Col xs={4} sm={4} md={4} lg={6} xl={6}> 
-                            
-                                <Button shape='round' className='done-btn'>done</Button>
-                            </Col>
-                            <Col xs={4} sm={4} md={4} lg={6} xl={6}>
-                                <Button shape='round' className='undone-btn'>undone</Button>
-                            </Col>
-                            <Col xs={4} sm={4} md={4} lg={6} xl={6}>
-                                <Button shape='round' danger className='delete-btn'>delete</Button>
-                            </Col>
-                            </Space>
-                        
-                        
-            
-                </Row>
+                                <Space align='center'>
+                                    {
+                                        ! probs.item.done  
+                                        ? <Col xs={4} sm={4} md={4} lg={6} xl={6}> 
+                                            <Button shape='round' className='done-btn' onClick={()=>probs.toggleaction(probs.item.key)}>done</Button>
+                                        </Col>
+                                        :
+                                        <Col xs={4} sm={4} md={4} lg={6} xl={6}>
+                                            <Button shape='round' className='undone-btn' onClick={()=>probs.toggleaction(probs.item.key)} >undone</Button>
+                                        </Col>
+                                    }
+                                    
+                                    <Col xs={4} sm={4} md={4} lg={6} xl={6}>
+                                        <Button shape='round' className='edit-btn' onClick={()=>setEditStatus(true)}>edit</Button>
+                                    </Col>
+                                    
+                                    <Col xs={4} sm={4} md={4} lg={6} xl={6}>
+                                        <Button shape='round' danger className='delete-btn' onClick={()=>probs.delete(probs.item.key)}>delete</Button>
+                                    </Col>
+                                    
+                                </Space>
+                            </Row>
+                           )
+                           :(
+                            <Row justify="space-around" align="middle" className='task-box'>
+                                <Col xs={24} sm={24} md={14} lg={16} xl={12}>
+                                    <Input onChange={inputHandler} value={editText} />
+                                </Col>
+
+                                <Space align='center'>
+                       
+                                    <Col xs={4} sm={4} md={4} lg={6} xl={6}>
+                                        <Button shape='round' className='edit-btn' onClick={()=>saveEditHandler(editText)} >save</Button>
+                                    </Col>
+                       
+                                </Space>
+                            </Row>
+                           )
+                       } 
+                
+         </>
            
     );
       }
